076259a8dd1ab53cf9435b821c7ebe4766d1dae6
NVS00445 <85628063+NVS00445@users.noreply.github.com>
2021-11-24 12:54:32 +0530

    VPC Peering - Updated latest files
    VPC Peering - Updated latest files

--- VPC-Peering/vpc_peering_main.tf
+++ VPC-Peering/vpc_peering_main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc_peering_connection" "vpc_peering" {
   peer_vpc_id   = var.peer_vpc_id
   vpc_id        = var.local_vpc_id
   peer_region   = var.peer_region
-  auto_accept   = false
+  auto_accept   = true
   tags = {
     Name = var.vpc_peering_name
   }
